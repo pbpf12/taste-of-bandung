5df4d47647bd3c5d26f26222e4956aedeb8a97f1
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2024-10-15 14:41:33 +0700

    feat: Add models for User and Dishes

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.username
+
+class Dishes(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    bookmark_count = models.IntegerField(default=0)
