a91c8483131fb47acc5566b592457007a1d2a859
rafiedel <rafie.asadel@ui.ac.id>
2024-10-24 19:44:36 +0700

    feat: Changes on url conf

--- search/urls.py
+++ search/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('search', show_search_page, name="search"),
-    path('get-dishes', get_dishes, name='get_dishes'),
+    path('search/', show_search_page, name="search"),
+    path('get-dishes/', get_dishes, name='get_dishes'),
 ]
