f48ff858d453611cdde9c6ef8a9f0bf822218801
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2024-10-26 00:43:08 +0700

    feat: Finalize userprofile.html features

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('delete/', delete_user, name='delete_user'),
     path('edit_profile/', edit_profile, name='edit_profile'),
     path('show_json/', show_json, name='show_json'),
+    path('show_history/', show_history_as_json, name='show_history'),
+    path('clear_history/', clear_history, name='clear_history'),
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
+from django.utils.html import strip_tags
+from main.models import History
 
 
 @login_required
@@ -23,6 +25,8 @@ def show_profile(request):
     if not user.first_name or not user.last_name or not user.email:
         messages.warning(request, "Your profile is incomplete. Please update your details.")
     
+
+    
     return render(request, "profile.html")
 
 def delete_user(request):
@@ -39,8 +43,8 @@ def edit_profile(request):
     if request.method == 'POST':
         try:
             user = request.user
-            user.first_name = request.POST.get('first_name', user.first_name)
-            user.last_name = request.POST.get('last_name', user.last_name)
+            user.first_name = strip_tags(request.POST.get('first_name', user.first_name))
+            user.last_name = strip_tags(request.POST.get('last_name', user.last_name))
             user.email = request.POST.get('email', user.email)
             user.save()
             
@@ -61,3 +65,28 @@ def edit_profile(request):
             }, status=400)
     
     return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=405)
+
+def show_history_as_json(request):
+    user = request.user
+    history = History.objects.filter(user=user).order_by('-created_at')
+
+    return JsonResponse({
+        'history': [
+            {
+                'dish': {
+                    'name': h.dish.name,
+                    'restaurant': h.dish.restaurant.name,
+                    'price': h.dish.price,
+                    'image': h.dish.image,
+                },
+                'created_at': h.created_at,
+            }
+            for h in history
+        ]
+    })
+
+def clear_history(request):
+    user = request.user
+    History.objects.filter(user=user).delete()
+
+    return redirect(reverse('show_history'))
