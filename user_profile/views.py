2bff67f2af6ce5afa3006fc3e525986917319894
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2024-10-26 18:43:07 +0700

    refactor: Remove unnecessary import in views.py

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
